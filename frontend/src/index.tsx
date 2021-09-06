297e59c90ac215eeb2de25d9025f48cb2249eb0d
Bruno Barbosa <brunobm19992014@gmail.com>
2021-09-07 00:58:42 +0100

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/styles.css';
 import App from './App';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-  
 );
